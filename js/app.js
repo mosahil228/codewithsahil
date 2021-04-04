84872fc2657c6eec079bb6476bcb3092e867bd63
mosahil228 <54403198+mosahil228@users.noreply.github.com>
2021-04-04 16:40:51 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,10 +30,13 @@ var typed = new Typed('.type', {
     backSpeed: 5,
     loop: true,
 });
+
+
 burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navlist = document.querySelector('.nav-list')
 burger.addEventListener('click', () => {
+    console.log("clicked")
     navbar.classList.toggle('h-nav');
     navlist.classList.toggle('v-class')
 
